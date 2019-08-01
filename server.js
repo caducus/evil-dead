8123f064d8cacba45feb2f1cb204947e3db8739a
Becka Catanzaro <becka@Beckas-MacBook-Pro.local>
2019-08-01 17:36:21 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,70 @@
+// testing to make sure env is connected
+// console.log(process.env.MONGODB_URI);
+
+// =====================
+// Dependencies
+// =====================
+
+const express = require('express');
+const methodOverride  = require('method-override');
+const mongoose = require ('mongoose');
+const app = express ();
+const db = mongoose.connection;
+
+// =====================
+// Port
+// =====================
+
+// Allow use of Heroku's port or your own local port, depending on the environment
+const port = process.env.PORT || 3000;
+
+// =====================
+// Database
+// =====================
+
+// How to connect to the database either via heroku or locally
+const MONGODB_URI = process.env.MONGODB_URI;
+
+// Fix Depreciation Warnings from Mongoose*
+// May or may not need these depending on your Mongoose version
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+// Connect to Mongo
+mongoose.connect(MONGODB_URI ,  { useNewUrlParser: true});
+
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+// =====================
+// Middleware
+// =====================
+
+//use public folder for static assets
+app.use(express.static('public'));
+
+// populates req.body with parsed info from forms - if no data from forms will return an empty object {}
+app.use(express.urlencoded({ extended: false }));// extended: false - does not allow nested objects in query strings
+app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
+
+//use method override
+app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
+
+// =====================
+// Routes
+// =====================
+
+//localhost:3000
+app.get('/' , (req, res) => {
+  res.send('Hello World!');
+});
+
+// =====================
+// Listener
+// =====================
+
+app.listen(port, () =>
+  console.log( 'Listening on port:', port
+));
